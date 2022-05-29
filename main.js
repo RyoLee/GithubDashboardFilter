e1cdb6cdf23c1a12e767d7f10bc2c3d63baf4854
Lee <516127941@qq.com>
2022-05-29 16:14:18 +0800

    release: version 1.0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  Minimizes pushs and commits from github actions and bots from github.com dashboard
 // @namespace    RyoLee
 // @author       RyoLee
-// @version      0.4
+// @version      1.0
 // @copyright    2022, RyoLee (https://github.com/RyoLee)
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @icon         https://github.githubassets.com/pinned-octocat.svg
