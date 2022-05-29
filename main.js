d10ef50501d52e9e7def0fcacbde6d6ed8f18509
Lee <516127941@qq.com>
2022-05-29 15:56:26 +0800

    remove: resize

--- main.js
+++ main.js
@@ -3,58 +3,47 @@
 // @description  Minimizes pushs and commits from github actions and bots from github.com dashboard
 // @namespace    RyoLee
 // @author       RyoLee
-// @version      0.1
+// @version      0.2
 // @copyright    2022, RyoLee (https://github.com/RyoLee)
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @match        https://github.com/
 // @updateURL    https://raw.githubusercontent.com/RyoLee/GithubDashboardFilter/master/main.js
+// @downloadURL  https://raw.githubusercontent.com/RyoLee/GithubDashboardFilter/master/main.js
 // @grant        none
 // ==/UserScript==
- 
+
 (function () {
     'use strict'
-   
+
     document.head.appendChild(document.createElement('style')).innerHTML = `
     .Details:hover .newexpanderbutton .Link--secondary {
       color: var(--color-accent-fg) !important;
     }
     `
-   
+
     function unhideBot (ev) {
       const div = this
       div.classList.add('shotBot')
       div.removeEventListener('click', unhideBot)
-      div.style.fontSize = ''
       if (div.querySelector('.no-border-bottom')) {
         div.querySelector('.no-border-bottom').classList.replace('no-border-bottom', 'border-bottom')
       }
-      div.querySelector('.Box').style.display = ''
-      div.querySelector('.body').style.height = ''
-      div.querySelector('.body .d-flex').style.padding = ''
-      div.querySelector('img.avatar').height = '32'
-      div.querySelector('img.avatar').width = '32'
       div.style.cursor = ''
       if (div.querySelector('.newexpanderbutton')) {
         div.querySelector('.newexpanderbutton').remove()
       }
     }
-   
+
     function hideBots () {
       const expandButton = document.querySelector('button.js-details-target:not(.Header-link)[aria-expanded="false"]')
       document.querySelectorAll('#dashboard div.push:not(.shotBot)').forEach(function (div) {
         const label = div.querySelector('.body .d-flex .d-flex .Label')
         const isAppUrl = div.querySelector('.body .d-flex .d-flex a.Link--primary[href^="/apps/"]')
         if (isAppUrl || (label && label.textContent === 'bot')) {
-          div.style.fontSize = '10px'
           if (div.querySelector('.border-bottom')) {
             div.querySelector('.border-bottom').classList.replace('border-bottom', 'no-border-bottom')
           }
-          div.querySelector('.Box').style.display = 'none'
-          div.querySelector('.body').style.height = '22px'
-          div.querySelector('.body .d-flex').style.padding = '0px'
-          div.querySelector('img.avatar').height = '20'
-          div.querySelector('img.avatar').width = '20'
           div.style.cursor = 'row-resize'
           div.addEventListener('click', unhideBot)
           const line = div.querySelector('.Details .flex-column .flex-justify-between.flex-items-baseline')
@@ -66,7 +55,7 @@
         }
       })
     }
-   
+
     hideBots()
     const iv = window.setInterval(hideBots, 200)
     window.setTimeout(() => window.clearInterval(iv), 5000)
