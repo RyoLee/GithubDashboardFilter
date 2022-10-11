3d645da067974518fa2422986d510d3d76f31e4c
Lee <516127941@qq.com>
2022-10-11 08:44:00 +0800

    fix: 适配前端属性变更

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  Minimizes pushs and commits from github actions and bots from github.com dashboard
 // @namespace    RyoLee
 // @author       RyoLee
-// @version      1.1
+// @version      1.2
 // @copyright    2022, RyoLee (https://github.com/RyoLee)
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @icon         https://github.githubassets.com/pinned-octocat.svg
@@ -17,7 +17,7 @@
   'use strict'
 
   function hideBots () {
-    document.querySelectorAll('#dashboard div.push:not(.shotBot), #dashboard div[classes="push"]:not(.shotBot)').forEach(function (div) {
+    document.querySelectorAll('#dashboard div.push:not(.shotBot), #dashboard div[classes*="push"]:not(.shotBot)').forEach(function (div) {
       const label = div.querySelector('.body .d-flex .d-flex .Label')
       const isAppUrl = div.querySelector('.body .d-flex .d-flex a.Link--primary[href^="/apps/"]')
       if (isAppUrl || (label && label.textContent === 'bot')) {
